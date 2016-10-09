50314c1c03c26e43f78f1a5ea5efd455e227e767
Luan <nguyenminhluan19051996@gmail.com>
2016-10-09 22:00:34 +0700

    commit in net

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 
-	var io = io.connect('http://localhost:3000');
+	var socket = io();
 
+	socket.on('send_message', function(msg) {
+		$("#chat_content").append($('<li>').append('span').text('Luan' + msg));
+	});
 
-
+	
+	$('form').submit(function() {
+		socket.emit('send_message', $("#chat_input").val());
+		$("#chat_input").val('');
+		return false;
+	});
+	
+	
 });
